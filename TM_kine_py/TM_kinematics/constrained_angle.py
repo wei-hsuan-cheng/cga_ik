85c91d8eb1470766267d085dbf4ce213924191e5
Wei-Hsuan Cheng <125959912+wei-hsuan-cheng@users.noreply.github.com>
2024-02-03 12:03:53 +0800

    Add files via upload

---
+++ TM_kine_py/TM_kinematics/constrained_angle.py
@@ -0,0 +1,22 @@
+import numpy as np
+from math import pi
+
+# This code is originally adapted from: https://github.com/dbddqy/visual_kinematics
+
+# ================== constrain angle between -pi and pi
+def simplify_angle(angle):
+    while angle > pi:
+        angle -= 2 * pi
+
+    while angle < -pi:
+        angle += 2 * pi
+
+    return angle
+
+
+# ================== constrain angles[n, ] between -pi and pi
+def simplify_angles(angles):
+    for i in range(angles.shape[0]): # extract each angle in the first (and only) column
+        angles[i] = simplify_angle(angles[i])
+
+    return angles
