17d2c5e8e1e5392d6ecb7330fb36dfd320b4407b
wei-hsuan-cheng <johnathancheng0125@gmail.com>
2025-04-12 14:47:07 +0800

    update config

--- config/calculate_geometric_params.py
+++ config/calculate_geometric_params.py
@@ -15,12 +15,12 @@ r_e = np.sqrt(r_s_epl**2 - d**2)
 
 
 # Print all parameters
-print(f"\nLower part params")
+print(f"\n[Lower part params]")
 print(f"r_c [m] = {r_c}")
 print(f"ang_b_m [deg] = {ang_b_m}")
 print(f"r_b [m] = {r_b}")
 
-print(f"\nUpper part params")
+print(f"\n[Upper part params]")
 print(f"d [m] = {d}")
 print(f"r_s_epl [m] = {r_s_epl}")
 print(f"r_e [m] = {r_e}")
